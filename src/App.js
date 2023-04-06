c3eb89b278b43799870c2abefb71f4670ebaf1a3
Nefari0 <chrisdemontigny@yahoo.com>
2023-04-06 09:14:34 -0600

    Plays text generated from strings imported from external .js file

--- src/App.js
+++ src/App.js
@@ -1,40 +1,66 @@
 import './App.css';
-import { useState } from 'react';
+import { useState,useReducer } from 'react';
+import { readString, readString2 } from './textString';
+
+const initialState = {isPlaying:false}
+function reducer(state, action) {
+  switch (action.type) {
+    case true:
+      return {isPlaying:true};
+    case false:
+      return {isPlaying:false};
+    default:
+      throw new Error();
+  }
+}
 
 function App() {
+  const [reducerState, dispatch] = useReducer(reducer, initialState);
+  const { isPlaying } = reducerState
   
   const [state,setState] = useState({
     currentItem:0,
-    delay:200,
-    arrayLength:20,
-    isPlaying:true
+    delay:200, // Milliseconds
   })
   const {
     currentItem,
     delay,
-    arrayLength,
-    isPlaying,
   } = state
-  const M = Array.from(Array(arrayLength)) // rows
-  const timedArray = M.map((el,i) => el=i)
+  // --- These array generators are for building and testing --- //
+  // const M = Array.from(Array(20))
+  // const timedArray = M.map((el,i) => el=i)
+  // ----------------------------------------------------------- //
 
-  const filteredText = timedArray.filter(item => item === currentItem)
+  const timedArray = readString.split('')
+  const filteredText = timedArray.filter((item,index) => index === currentItem)
+  const mappedText = filteredText.map((el,i) => {return <h2 key={i}>{el}</h2>})
 
-  const mappedText = filteredText.map((el,i) => {
-      return <h2 key={i}>here is the element:{el}</h2>
-    })
-
-  const delayControler = () => {
+  const delayControler = async () => {
+    dispatch({type:true})
     timedArray.forEach((el,i) => {
-      (function (el) {
-        setTimeout(function () {setState({...state,currentItem:i})}, delay * el);
-      })(el);
+      
+      (function (i) {
+        setTimeout(function () {
+          if (i === timedArray.length-1) {
+            dispatch({type:false})
+          } else {
+            setState({...state,currentItem:i})
+          }
+        }, delay * i);
+      })(i);
     })
   }
 
   return (
+    
     <div className="App">
-      <button onClick={() => delayControler()}>{isPlaying ? 'stop' : 'start'}</button>
+      <div style={{height:'25px',width:'50px',margin:'auto'}}>
+        {isPlaying === false && 
+        <button onClick={() => delayControler()}>
+          start
+        </button>}
+      </div>
+      {/* <button onClick={() => dispatch({type: !isPlaying})}>start2</button> */}
       {mappedText}
     </div>
   );
