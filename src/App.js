94dfd1e19b8dc6985d6221fb2a2a70d36004aa58
Nefari0 <chrisdemontigny@yahoo.com>
2023-04-06 20:21:41 -0600

    Paste text from copied sources

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState,useReducer } from 'react';
 import { readString, readString2 } from './textString';
+import TextEditor from './lib/TextBox/text-box.component';
 
 const initialState = {isPlaying:false}
 function reducer(state, action) {
@@ -21,19 +22,52 @@ function App() {
   const [state,setState] = useState({
     currentItem:0,
     delay:200, // Milliseconds
+    stateText:readString
   })
   const {
     currentItem,
     delay,
+    stateText
   } = state
   // --- These array generators are for building and testing --- //
   // const M = Array.from(Array(20))
   // const timedArray = M.map((el,i) => el=i)
   // ----------------------------------------------------------- //
 
-  const timedArray = readString.split(' ')
+  const timedArray = stateText.split(' ')
   const filteredText = timedArray.filter((item,index) => index === currentItem)
   const mappedText = filteredText.map((el,i) => {return <h2 key={i}>{el}</h2>})
+
+  const inputHandler = (e) => {
+    const { name,value } = e.target
+    console.log('hit input handler',name)
+    setState({...state,[name]:value})
+  }
+
+  const pasteFromClipboard = (e) => {
+        e.preventDefault()
+        // try {
+          
+          navigator.clipboard.readText()
+          .then(text => {
+            // --- verify that copied items are integers or floats in string format --- //
+            try {
+              if (typeof(text) === 'string') {
+                        console.log('hit copy function',typeof text)
+                        setState({
+                          ...state,
+                          stateText:text
+                      })  
+                    } 
+                   
+                  } catch (error) {
+                      console.log(error)
+                  }
+                })
+                .catch(err => {
+                    console.log(null);
+                });
+    }
 
   const delayControler = async () => {
     dispatch({type:true})
@@ -54,7 +88,8 @@ function App() {
   return (
     
     <div className="App">
-      <textarea  />
+      {/* <TextEditor inputHandler={inputHandler} text={stateText}/> */}
+      <button onClick={(e) => pasteFromClipboard(e)}>paste text</button>
       <div style={{height:'25px',width:'50px',margin:'auto'}}>
         {isPlaying === false && 
         <button onClick={() => delayControler()}>
