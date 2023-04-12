e42a24e6d0bcdbc7c34b3fd7f5977b21e83db14f
Nefari0 <chrisdemontigny@yahoo.com>
2023-04-11 23:13:16 -0600

    Text is rendered in seperate component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { useState,useReducer,useEffect } from 'react';
 import { readString, readString2 } from './textString';
 
+import TextLine from './Components/text-line/textline.components';
+
 const initialState = {
   chunkIndex:0,
   segments:[],
@@ -9,16 +11,8 @@ const initialState = {
 }
 function reducer(state, action) {
   switch (action.type) {
-    case 'index+':
-      var newVal = state.chunkIndex+=1
-      return {...initialState,chunkIndex:newVal};
-    case 'index-':
-      var newVal = state.chunkIndex-=1
-      return {...initialState,chunkIndex:newVal};
     case 'update':
       return {...initialState,segments:action.payload}
-    case 'start':
-      return {...initialState,isPlaying:action.payload}
     default:
       throw new Error();
   }
@@ -26,7 +20,6 @@ function reducer(state, action) {
 
 function App() {
   const [reducerState, dispatch] = useReducer(reducer, initialState);
-  const { segments } = reducerState
   
   const [state,setState] = useState({
     currentItem:0, // Segment index currently displayed
@@ -35,12 +28,10 @@ function App() {
     splitOption:' ', // View words or letters
     wordsPerSegment:3, // Number of words visible at a single time
     chunkSize:5, // Determining how many slice elements are pushed to array.
-    chunks:[],
+    chunks:null,
     chunkIndex:0,
   })
   const {
-    currentItem,
-    delay,
     stateText,
     splitOption,
     wordsPerSegment,
@@ -76,10 +67,6 @@ function App() {
     }
   }
 
-  // --- For displaying multiple words --- //
-  const filteredSegments = segments.filter((item,index) => index === currentItem)
-  const mappedWords = filteredSegments.map((el,i) => {return <h2 key={i}>{el}</h2>})
-
   const pasteFromClipboard = (e) => {
     e.preventDefault()
       navigator.clipboard.readText()
@@ -95,58 +82,21 @@ function App() {
     });
   }
 
-  const startPlaying = () => {
-    
-    segments.forEach((el,index) => {
-      setTimeout(function () {
-          var updateIndexValue = () => {
-            return (index === chunkSize ? 0 : index)
-          }
-          setState({
-            ...state,
-            currentItem:updateIndexValue(),
-            chunkIndex:chunkIndex+(chunkSize/chunkSize),
-          })        
-      }, delay * index);
-    })
-    if (chunkIndex <= chunks.length-2) {dispatch({type:'update',payload:chunks[chunkIndex+1],isPlaying:false})}
-  }
-
-  const sectionManager = (param) => {
-    switch (param) {
-      case 'play':
-        return startPlaying()
-      case 'prev':
-        dispatch({type:'update',payload:chunks[chunkIndex-1]})
-        setState({
-          ...state,
-          currentItem:0,
-          chunkIndex:chunkIndex-1
-        })
-        return
-    }
-  }
-
   return (
     
     <div className="App">
 
       <button onClick={(e) => pasteFromClipboard(e)}>paste text</button>
-      <div style={{height:'25px',width:'50px',margin:'auto'}}>
 
-        {chunkIndex < 1 ?
-          <button onClick={() => sectionManager('play')}>
-            start
-          </button>
-          :
-          <div style={{display:'flex'}}>
-            <button onClick={() => sectionManager('prev')}>previous</button>
-            <button onClick={() => sectionManager('play')}>next</button>
-          </div>
-        }
-
-      </div>
-      {mappedWords}
+      {chunks &&
+        
+        <TextLine
+          elements={chunks[chunkIndex]}
+          state={state}
+          setState={setState}
+        />
+      }
+      
     </div>
   );
 }
---
+++ src/Components/text-line/textline.components.jsx
@@ -0,0 +1,70 @@
+import { useEffect,useReducer } from "react";
+
+const initialState = {
+    slice:0,
+    segments:[],
+    isPlaying:false,
+  }
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'count':
+            return {...initialState,slice:action.payload}
+      default:
+        throw new Error();
+    }
+  }
+
+const TextLine = ({elements,state,setState}) => {
+
+    const { delay,chunkIndex } = state
+
+
+    const [reducerState, dispatch] = useReducer(reducer, initialState);
+    const { slice } = reducerState
+
+    const filteredElements = elements.filter((el,i) => i === slice)
+    const mappedElements = filteredElements.map((el,i) => {
+        return <p key={i}>{el}</p>
+    })
+
+    useEffect(() => {timer()},[elements])
+
+    const timer = () => {
+        elements.forEach((el,index) => {
+            setTimeout(function () {
+                dispatch({type:'count',payload:slice+index})
+            }, delay * index);
+        })
+    }
+
+    const readActionHander = (i) => {
+        if (chunkIndex + i > 0 && chunkIndex + i < state.chunks.length) {
+            dispatch({type:'count',payload:0})
+            setState({...state,chunkIndex:state.chunkIndex+i})
+        }
+    
+    }
+    
+    return (
+        <div>
+            {mappedElements}
+            <div style={{display:'flex',width:'100vw'}}>
+            <button
+                onClick={() => readActionHander(-1)}
+                style={{margin:'auto'}}>
+                prev
+            </button>
+          
+            <button
+                onClick={() => readActionHander(1)}
+                style={{margin:'auto'}}
+            >
+                next
+            </button>
+        </div>
+        </div>
+    )
+}
+
+export default TextLine
