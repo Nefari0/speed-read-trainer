5ad0733391b553252f227bf846aabe4e770b6dd3
Nefari0 <chrisdemontigny@yahoo.com>
2023-04-18 14:51:15 -0600

    react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { register as registerServiceWorker } from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
+const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
